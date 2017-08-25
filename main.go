f362a1bdcbbf025aeb124a234e336244f2e2e8a8
Andreas Auernhammer <aead@mail.de>
2017-08-25 13:57:59 -0700

    fix help and error messages

--- main.go
+++ main.go
@@ -30,9 +30,9 @@ func main() {
 		return
 	case "enc":
 		enc := flag.NewFlagSet("enc", flag.ExitOnError)
-		pwd := enc.String("pwd", "", "The password used derive a encryption key and encrypt the data")
-		salt := enc.String("salt", "", "The salt used to derive an encryption key from the password")
-		key := enc.String("key", "", "The encryption key used to encrypt data - cannot be used with a password and must be 256 bit key as HEX")
+		pwd := enc.String("pwd", "", "The password used derive a secret key and encrypt the data")
+		salt := enc.String("salt", "", "The salt used to derive a secret key from the password")
+		key := enc.String("key", "", "The secret key used to encrypt data - cannot be used with a password and must be 256 bit key as HEX")
 		if err := enc.Parse(os.Args[2:]); err != nil {
 			fmt.Println(err)
 			return
@@ -40,9 +40,9 @@ func main() {
 		encrypt(*pwd, *salt, *key)
 	case "dec":
 		dec := flag.NewFlagSet("dec", flag.ExitOnError)
-		pwd := dec.String("pwd", "", "The password used derive a encryption key and encrypt the data")
-		salt := dec.String("salt", "", "The salt used to derive an encryption key from the password")
-		key := dec.String("key", "", "The encryption key used to encrypt data - cannot be used with a password and must be 256 bit key as HEX")
+		pwd := dec.String("pwd", "", "The password used derive a secret key and decrypt the data")
+		salt := dec.String("salt", "", "The salt used to derive a secret key from the password")
+		key := dec.String("key", "", "The secret key used to decrypt data - cannot be used with a password and must be 256 bit key as HEX")
 		if err := dec.Parse(os.Args[2:]); err != nil {
 			fmt.Println(err)
 			return
@@ -53,24 +53,25 @@ func main() {
 
 func encrypt(pwd, salt, key string) {
 	if pwd != "" && key != "" {
-		fmt.Println("Cannot use password and encryption key")
+		fmt.Println("Cannot use password and secret key")
 		return
 	}
 	if key != "" {
 		encKey, err := hex.DecodeString(key)
 		if err != nil {
-			fmt.Println("Failed to parse encryption key - Is the key not a HEX value?")
+			fmt.Println("Failed to parse secret key - Is the key not a HEX value?")
 			return
 		}
 		if _, err = aead.Encrypt(os.Stdout, os.Stdin, aead.Config{Key: encKey}); err != nil {
 			fmt.Println("Failed to encrypt data: ", err)
 			return
 		}
+		return
 	}
 	if pwd != "" {
 		key, err := scrypt.Key([]byte(pwd), []byte(salt), 16384, 8, 1, 32)
 		if err != nil {
-			fmt.Println("Failed to derive encryption key from password and salt: ", err)
+			fmt.Println("Failed to derive secret key from password and salt: ", err)
 			return
 		}
 		if _, err = aead.Encrypt(os.Stdout, os.Stdin, aead.Config{Key: key}); err != nil {
@@ -86,24 +87,25 @@ func encrypt(pwd, salt, key string) {
 
 func decrypt(pwd, salt, key string) {
 	if pwd != "" && key != "" {
-		fmt.Println("Cannot use password and decryption key")
+		fmt.Println("Cannot use password and secret key")
 		return
 	}
 	if key != "" {
 		encKey, err := hex.DecodeString(key)
 		if err != nil {
-			fmt.Println("Failed to parse decryption key - Is the key not a HEX value?")
+			fmt.Println("Failed to parse secret key - Is the key not a HEX value?")
 			return
 		}
 		if _, err = aead.Decrypt(os.Stdout, os.Stdin, aead.Config{Key: encKey}); err != nil {
 			fmt.Println("Failed to decrypt data: ", err)
 			return
 		}
+		return
 	}
 	if pwd != "" {
 		key, err := scrypt.Key([]byte(pwd), []byte(salt), 16384, 8, 1, 32)
 		if err != nil {
-			fmt.Println("Failed to derive decryption key from password and salt: ", err)
+			fmt.Println("Failed to derive secret key from password and salt: ", err)
 			return
 		}
 		if _, err = aead.Decrypt(os.Stdout, os.Stdin, aead.Config{Key: key}); err != nil {
